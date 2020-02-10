12866a76f6f0f26ac166788638f112860fc192f1
Tommy Connor <tconnor98@aol.com>
2020-02-10 22:04:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ class TicTacToe
     
   def valid_move?(position)
     position = position.to_i - 1
-    if position.between?(0,8) && !position_taken?(position)
+    if position.between?(0,8) && position_taken?(position)
       true
     else
       false
