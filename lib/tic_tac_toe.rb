59b6a6b5622bd64d2ddefcf6f39e0ba376d6dbb5
Tommy Connor <tconnor98@aol.com>
2020-02-10 21:18:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class TicTacToe
   end
     
   def move(location, current_player = "X")
-    @board[location] = current_player
+    @board[location.index] = current_player
   end
     
   def position_taken?(position)
